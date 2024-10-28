d0f0e815fb670c5c0b4077dd4be600d1779c55cd
aammddkkzxc <ideal0427@naver.com>
2024-10-28 20:04:30 +0900

    feat: 인터럽트 - 시작2 - 인터럽트 상태를 정상으로 변경해주지 않으면 이후 원하지 않는 때 (예 자원 정리 중)에 인터럽트 예외가 발생하여 작업을 마무리 할 수 없게 될수도 있음

---
+++ src/thread/control/interrupt/ThreadStopMainV3.java
@@ -0,0 +1,43 @@
+
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV3 {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task);
+        thread.start();
+
+        sleep(1000);
+        log("작업 중단 지시 thread.interrupt()");
+        thread.interrupt();
+        log("work 스레드 인터럽트 상태1 = " + thread.isInterrupted());
+    }
+
+    static class MyTask implements Runnable {
+        @Override
+        public void run() {
+
+            while (!Thread.currentThread().isInterrupted()) {
+                log("작업중");
+            }
+            log("work 스레드 인터럽트 상태2 = " + Thread.currentThread().isInterrupted());
+
+            try {
+                log("자원 정리 시도");
+                Thread.sleep(3000);
+                log("자원 정리 완료");
+            } catch (InterruptedException e) {
+                log("자원 정리 실패 - 자원 정리 중 인터럽트 발생");
+                log("work 스레드 인터럽트 상태3 = " + Thread.currentThread().isInterrupted());
+                log("interrupt message=" + e.getMessage());
+                log("state=" + Thread.currentThread().getState());
+            }
+
+            log("종료");
+        }
+    }
+}
